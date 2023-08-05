4d565e072f31c1c2a9480fdd839dfcb36bf4abd6
Madhusmita999995 <mama.glamour95@gmail.com>
2023-08-05 16:57:59 +0530

    new file TC_02 added

---
+++ src/testcases/TC_02_NewFile.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_02_NewFile 
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("demo file" , true);
+	}
+}
